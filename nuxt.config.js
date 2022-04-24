b5869d6847b85ff942fd5bc8c1072c87243bc212
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-04-24 18:47:45 +0300

    nuxt.config.js: light theme added

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,8 +62,10 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
     theme: {
-      dark: true,
+      dark: false,
+      light: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -73,6 +75,12 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
+        },
+        light: {
+          primary: '#db4b0d',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c',
         },
       },
     },
