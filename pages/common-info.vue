e660ed78a38b7e41f56aefa70d9ff00f0dfd5b2f
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-04-30 20:16:39 +0300

    Pages created

---
+++ pages/common-info.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      Common info
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'CommonInfoPage',
+  }
+</script>
---
+++ pages/news.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-row>
+        <v-col cols="12">
+            News page
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'NewsPage',
+  }
+</script>
