1879c750e5808a79df3afd3555dc879117c9d3dc
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-04-30 20:17:11 +0300

    Pages added to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,8 +25,8 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <v-app-bar-nav-icon @click.stop="switchDrawer" />
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
@@ -34,7 +34,7 @@
       </v-btn>
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
@@ -60,19 +60,29 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/',
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          title: 'Common info',
+          to: '/common-info',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'News',
+          to: '/news',
         },
       ],
       miniVariant: false,
       right: true,
-      title: 'Vuetify.js',
+      title: 'Dopomoga',
     }
+  },
+  methods: {
+    switchDrawer() {
+      this.drawer = !this.drawer;
+    },
   },
 }
 </script>
