6d226e5c0cede5f9d40d713326845fe6682cbac8
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-05-01 17:34:17 +0300

    Updates

--- _nuxt/static/1651415485/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcommon-info","\u002Fnews"]})
---
+++ _nuxt/static/1651415654/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1651415654",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c56b4529"}}}}(null));
