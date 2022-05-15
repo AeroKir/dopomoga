a29f6ee90d9122c7484c4bbd950792da6ade1b79
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-05-15 22:08:20 +0300

    Main page hero paragraph&translations

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <h1>{{ $t('pages.main.title') }}</h1>
+      <p>{{ $t('pages.main.heroParagraph', {siteName: 'Dopomoga'}) }}</p>
     </v-col>
   </v-row>
 </template>
