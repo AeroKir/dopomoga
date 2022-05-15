affd7360e0c3590f2dd5c345c106c6a37e830dd3
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-05-15 23:15:56 +0300

    Index page: added main info cards

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,23 @@
     <v-col cols="12" sm="8" md="6">
       <h1>{{ $t('pages.main.title') }}</h1>
       <p>{{ $t('pages.main.heroParagraph', {siteName: 'Dopomoga'}) }}</p>
+      <v-card>
+        <v-card-title>
+          Отримати довідку переселенця
+        </v-card-title>
+      </v-card>
+      <v-card>
+        <v-card-title>Отримати медичну допомогу</v-card-title>
+      </v-card>
+      <v-card>
+        <v-card-title>Отримати грошову допомогу</v-card-title>
+      </v-card>
+      <v-card>
+        <v-card-title>Безкоштовна правова допомога</v-card-title>
+      </v-card>
+      <v-card>
+        <v-card-title>Житло</v-card-title>
+      </v-card>
     </v-col>
   </v-row>
 </template>
