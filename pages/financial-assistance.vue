e1b3751368f3689a5dde032e6806e0537b4947af
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-06-05 19:35:55 +0300

    financial-assistance page: text added

--- pages/financial-assistance.vue
+++ pages/financial-assistance.vue
@@ -2,6 +2,28 @@
   <v-row>
     <v-col class="text-center">
       Get financial Assistance
+      <p>
+        Наразі, внаслідок агресивного і повномасштабного вторгнення Росії в Україну, склалася вкрай складна ситуація у нашій державі і майже 10 млн. наших співвітчизників є внутрішньо переміщеними особами і знаходяться в скрутному становищі.
+На порталі ДОПОМОГА, Ви маєте змогу знайти необхідну інформацію про янаявні програми підтримки від держави Україна а також від міжнародних партнерів.
+
+З наявною грошовою допомогою для ВПО можно ознаймитись за цими посиланнями:
+
+https://minre.gov.ua/news/vpo-mozhut-otrymaty-mizhnarodnu-groshovu-dopomogu-yaki-programy-dostupni
+
+https://www.unhcr.org/ua/43231-unhcr-is-launching-a-cash-assistance-programme-for-idps-ua.html
+
+https://visitukraine.today/uk/blog/440/finansova-dopomoga-vnutrisnyo-peremishhenim-osobam-vid-norvezkoi-radi-yak-otrimati-2200-griven
+
+https://fakty.com.ua/ua/ukraine/suspilstvo/20220601-finansova-dopomoga-vpo-vid-nrc-hto-ta-yak-zmozhe-otrymaty/
+
+https://www.prostir.ua/?news=vpo-mozhut-otrymaty-mizhnarodnu-hroshovu-dopomohu-yaki-prohramy-dostupni
+
+
+Контакти організації:
+© ГО СОБОРНІСТЬ 2020
+  sobornist.sobornist@gmail.com
+ Юрист - +38 095 898 66 43
+      </p>
     </v-col>
   </v-row>
 </template>
