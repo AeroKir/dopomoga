c4b6dd4586b2c864767dc4264d74402b9138ef8c
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-06-14 21:07:04 +0300

    financial--assistance page: links combined via expansion panels

--- pages/financial-assistance.vue
+++ pages/financial-assistance.vue
@@ -12,41 +12,33 @@
       <h1>Get financial Assistance</h1>
     </v-col>
     <v-col cols="11">
-      <v-expansion-panels>
-        <v-expansion-panel
-          v-for="(item,i) in 5"
-          :key="i"
-        >
-          <v-expansion-panel-header>
-            Item
-          </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
       <p>
         Наразі, внаслідок агресивного і повномасштабного вторгнення Росії в Україну, склалася вкрай складна ситуація у нашій державі і майже 10 млн. наших співвітчизників є внутрішньо переміщеними особами і знаходяться в скрутному становищі.
-На порталі ДОПОМОГА, Ви маєте змогу знайти необхідну інформацію про янаявні програми підтримки від держави Україна а також від міжнародних партнерів.
-
-З наявною грошовою допомогою для ВПО можно ознаймитись за цими посиланнями:
-
-https://minre.gov.ua/news/vpo-mozhut-otrymaty-mizhnarodnu-groshovu-dopomogu-yaki-programy-dostupni
-
-https://www.unhcr.org/ua/43231-unhcr-is-launching-a-cash-assistance-programme-for-idps-ua.html
-
-https://visitukraine.today/uk/blog/440/finansova-dopomoga-vnutrisnyo-peremishhenim-osobam-vid-norvezkoi-radi-yak-otrimati-2200-griven
-
-https://fakty.com.ua/ua/ukraine/suspilstvo/20220601-finansova-dopomoga-vpo-vid-nrc-hto-ta-yak-zmozhe-otrymaty/
-
-https://www.prostir.ua/?news=vpo-mozhut-otrymaty-mizhnarodnu-hroshovu-dopomohu-yaki-prohramy-dostupni
-
-
+        На порталі ДОПОМОГА, Ви маєте змогу знайти необхідну інформацію про янаявні програми підтримки від держави Україна а також від міжнародних партнерів.
+        З наявною грошовою допомогою для ВПО можно ознаймитись за цими посиланнями:
 Контакти організації:
 © ГО СОБОРНІСТЬ 2020
   sobornist.sobornist@gmail.com
  Юрист - +38 095 898 66 43
       </p>
+      <v-expansion-panels>
+        <v-expansion-panel
+          v-for="(assistanceItem,i) in financialAssistanceSources"
+          :key="i"
+        >
+          <v-expansion-panel-header>
+            {{ assistanceItem.assistanceSource }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-list-item
+              v-for="(link, i) in assistanceItem.assistanceLinks"
+              :key="i"
+            >
+              <a :href="link" target="_blank" rel="noopener noreferrer">{{ link }}</a>    
+            </v-list-item>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-col>
   </v-row>
 </template>
@@ -54,5 +46,26 @@ https://www.prostir.ua/?news=vpo-mozhut-otrymaty-mizhnarodnu-hroshovu-dopomohu-y
 <script>
   export default {
     name: 'FinancialAssistancePage',
+    data() {
+      return {
+        financialAssistanceSources: [
+          {
+            assistanceSource: 'Допомога від держави',
+            assistanceLinks: [
+              'https://minre.gov.ua/news/vpo-mozhut-otrymaty-mizhnarodnu-groshovu-dopomogu-yaki-programy-dostupni'
+            ],
+          },
+          {
+            assistanceSource: 'Допомога від партнерів',
+            assistanceLinks: [
+              'https://www.unhcr.org/ua/43231-unhcr-is-launching-a-cash-assistance-programme-for-idps-ua.html',
+              'https://visitukraine.today/uk/blog/440/finansova-dopomoga-vnutrisnyo-peremishhenim-osobam-vid-norvezkoi-radi-yak-otrimati-2200-griven',
+              'https://fakty.com.ua/ua/ukraine/suspilstvo/20220601-finansova-dopomoga-vpo-vid-nrc-hto-ta-yak-zmozhe-otrymaty/',
+              'https://www.prostir.ua/?news=vpo-mozhut-otrymaty-mizhnarodnu-hroshovu-dopomohu-yaki-prohramy-dostupni'
+            ],
+          },
+        ],
+      };
+    },
   }
 </script>
