ddc3dd9aeac10e85e88411dc34c8538c00898a2b
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-06-14 21:08:55 +0300

    Updates

--- _nuxt/static/1654457763/en/stop-fake/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1654457763",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fstop-fake",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3964f4df"}}}}(null));
