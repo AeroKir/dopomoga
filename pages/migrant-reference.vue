3190ac5848a3bf63a6a7b66b70b11a6b45d38b29
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-06-19 17:32:51 +0300

    migrant reference page: basr markup

--- pages/migrant-reference.vue
+++ pages/migrant-reference.vue
@@ -1,9 +1,23 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      Get migrant reference
-    </v-col>
-  </v-row>
+  <v-container fluid>
+        <v-row class="justify-center mt-1">
+            <v-col cols="12" sm="10" lg="8" class="d-flex">
+                <nuxt-link to="/" style="text-decoration: none">
+                <v-icon large color="#0461ab">mdi-arrow-left</v-icon>
+                </nuxt-link>
+                <v-icon color="#fed82f" x-large class="mx-auto" style="transform: translateX(-20px);">mdi-card-account-details</v-icon>
+            </v-col>
+
+            <v-col cols="12" class="text-center">
+                <h1>Get migrant reference</h1>
+            </v-col>
+            <v-col cols="12" sm="10" lg="8" class="mx-auto">
+                <p class="">
+                
+                </p>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
