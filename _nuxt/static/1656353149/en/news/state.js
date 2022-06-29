3dc4a827666e2d2b0aaeda8903924e01b049eaf2
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-06-29 21:16:42 +0300

    Updates

--- _nuxt/static/1656353149/en/news/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1656353149",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fnews",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"802ba19c"}}}}(null));
