d07f12f0cf4b5bd687693b6dd07f3668f3157bf7
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-07-03 21:04:07 +0300

    Legal assistance translations&styles

--- pages/legal-assistance.vue
+++ pages/legal-assistance.vue
@@ -12,9 +12,15 @@
                 <h1>{{ $t('pages.legalAssistance.title') }}</h1>
             </v-col>
             <v-col cols="12" sm="10" lg="8" class="mx-auto">
-                <p>
-                    Юрист - <a href="tel:+380958986643">+38 095 898 66 43</a>
-                </p>
+                <section class="mb-10">
+                    <p>{{ $t('pages.legalAssistance.infoParagraph.infoBlock1') }}</p>
+                    <h3 class="mb-5 mt-5 text-center">{{ $t('pages.legalAssistance.infoParagraph.subtitle1') }}</h3>
+                    <h2 class="d-flex justify-center align-center font-weight-bold text-center">
+                        <v-icon x-large color="#0461ab" class="mr-2">mdi-cellphone-information</v-icon>
+                        <a href="tel:+380958986643" style="color: #0461ab;">+38 095 898 66 43</a>
+                    </h2>
+                </section>
+                
                 <v-row>
                     <v-col cols="12" sm="12" lg="12">
                         <SocialLinks class="mt-3" />
