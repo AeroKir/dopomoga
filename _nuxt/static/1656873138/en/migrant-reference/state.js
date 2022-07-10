aeb11c4fbe09bf129542d45a2a28cfa4ccf9842b
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-07-10 19:53:57 +0300

    Updates

--- _nuxt/static/1656873138/en/migrant-reference/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1656873138",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fmigrant-reference",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb4f9c3f"}}}}(null));
