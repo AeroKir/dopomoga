4931cee298b73f755c73e0566c785939a04611ec
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-07-10 19:57:40 +0300

    Updates

--- _nuxt/static/1657472029/ru/legal-assistance/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1657472029",layout:"default",error:a,serverRendered:true,routePath:"\u002Fru\u002Flegal-assistance",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"336301c2"}}}}(null));
