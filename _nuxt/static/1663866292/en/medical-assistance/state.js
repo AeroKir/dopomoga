611e94b2e9b649c1765a2b1ac3d26224ba8c446b
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-09-22 20:07:27 +0300

    Updates

--- _nuxt/static/1663866292/en/medical-assistance/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663866292",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fmedical-assistance",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b928b389"}}}}(null));
