ffdb73c7ee340666f5ddcb8470d1a3cc2ce18084
Kirill Shevtsov <shev.kv.1982@gmail.com>
2022-09-22 20:10:59 +0300

    Updates

--- _nuxt/static/1663866438/medical-assistance/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1663866438",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmedical-assistance",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dcf34b92"}}}}(null));
---
+++ _nuxt/static/1663866650/en/legal-assistance/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdopomoga\u002F_nuxt\u002Fstatic\u002F1663866650",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Flegal-assistance",config:{_app:{basePath:"\u002Fdopomoga\u002F",assetsPath:"\u002Fdopomoga\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8e35dc82"}}}}(null));
